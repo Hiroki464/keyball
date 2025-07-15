30c57333ecf9285255d437beff0d2a069c373d75
Hiroki464 <147540173+Hiroki464@users.noreply.github.com>
2025-07-16 02:35:19 +0900

    マウスレイヤー修正

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/hiroki/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/hiroki/keymap.c
@@ -70,8 +70,9 @@ void oledkit_render_info_user(void) {
 }
 #endif
 
-// in keymap.c:
+// オートマウスを有効にする
+#ifdef POINTING_DEVICE_AUTO_MOUSE_ENABLE
 void pointing_device_init_user(void) {
-    set_auto_mouse_layer(2);
     set_auto_mouse_enable(true);
 }
+#endif
