d26f7ece0adf8de542e8d76a61ff058be522ba6c
Hiroki464 <147540173+Hiroki464@users.noreply.github.com>
2025-07-16 12:22:23 +0900

    [upd]OLED更新

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/hiroki/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/hiroki/keymap.c
@@ -68,7 +68,7 @@ void oledkit_render_info_user(void) {
     //keyball_oled_render_ballinfo();
     //keyball_oled_render_layerinfo();
 
-    oled_write_ln(PSTR("1line"), false);
+    oled_write_P(PSTR("Hello"), false);
     //oled_write_ln(PSTR("2line"), false);
     //oled_write_ln(PSTR("3line"), false);
     //oled_write_ln(PSTR("4line"), false);
