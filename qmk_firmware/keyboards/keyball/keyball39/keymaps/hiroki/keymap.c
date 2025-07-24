76c773f17ca96fa0249cc5795b198019a0845a2a
Hiroki464 <147540173+Hiroki464@users.noreply.github.com>
2025-07-24 17:46:13 +0900

    [upd] キーコンボ

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/hiroki/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/hiroki/keymap.c
@@ -118,12 +118,14 @@ const uint16_t PROGMEM mcomm_combo[]  = {KC_M, KC_COMM, COMBO_END}; // M,コン�
 const uint16_t PROGMEM qwer_combo[]   = {KC_Q, KC_W, KC_E, KC_R, COMBO_END}; // Q,W,E,Rコンボ
 const uint16_t PROGMEM fd_combo[]     = {KC_F, KC_D, COMBO_END}; // F,Dコンボ
 const uint16_t PROGMEM fs_combo[]     = {KC_F, KC_S, COMBO_END}; // F,Sコンボ
+const uint16_t PROGMEM kl_combo[]     = {KC_K, KC_L, COMBO_END}; // K,Lコンボ
 
 combo_t key_combos[] = {
 COMBO(fd_combo, KC_BTN1)      // F,Dコンボで左クリック
 ,COMBO(fs_combo, KC_BTN2)     // F,Sコンボで右クリック
 ,COMBO(cv_combo, KC_LNG2)     // C,Vコンボで言語2を選択
 ,COMBO(mcomm_combo, KC_LNG1)  // M,コンマコンボで言語1を選択
+,COMBO(kl_combo, KC_Z)        // K,LコンボでZキー
 ,COMBO(sd_combo, SCRL_MO)     // S,Dコンボでスクロールモード（SCRL_MO）
 ,COMBO(qwer_combo, TG(3))     // Q,W,E,Rコンボでレイヤー3に切替
 };
